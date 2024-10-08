b3efb2c491052907051354aef8dbff28d5280164
YGN <ygntechstartup@gmail.com>
2024-10-08 16:49:28 +0530

    any val

--- components/ui/images-slider.tsx
+++ components/ui/images-slider.tsx
@@ -21,7 +21,7 @@ export const ImagesSlider = ({
   direction?: "up" | "down";
 }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
   const [loadedImages, setLoadedImages] = useState<string[]>([]);
 
   const handleNext = () => {
@@ -41,7 +41,7 @@ export const ImagesSlider = ({
   }, []);
 
   const loadImages = () => {
-    setLoading(true);
+    // setLoading(true);
     const loadPromises = images.map((image) => {
       return new Promise((resolve, reject) => {
         const img = new Image();
@@ -54,7 +54,7 @@ export const ImagesSlider = ({
     Promise.all(loadPromises)
       .then((loadedImages) => {
         setLoadedImages(loadedImages as string[]);
-        setLoading(false);
+        // setLoading(false);
       })
       .catch((error) => console.error("Failed to load images", error));
   };
